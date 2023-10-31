f62e408d13b5d2c97380ffe2e95a8f01632f8cd5
Ugwu Friday B <fugwungene@gmail.com>
2023-10-31 15:24:17 +0000

    My first commit
    Add project files
    Code task zero

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - creates a new binary tree
+ * @parent: parent node
+ * @value: value
+ * Return: pointer to the new node
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	new_node = malloc(sizeof(*new_node));
+	if (new_node == NULL)
+	{
+		return(NULL);
+	}
+	new_node->parent = parent;
+	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+
+	return(new_node);
+}
