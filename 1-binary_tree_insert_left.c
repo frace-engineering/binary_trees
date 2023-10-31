ae3674341dfb22be08ed4bf0bb9d1332a669720c
Ugwu Friday B <fugwungene@gmail.com>
2023-10-31 20:59:12 +0000

    Add code lines to task 2 and modify task 1

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,35 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - creates a new binary tree
+ * @parent: parent node
+ * @value: value
+ * Return: pointer to the new node
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+{
+	binary_tree_t *new_node;
+
+	if (parent == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(*new_node));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	if (parent->left != NULL)
+	{
+		new_node->left = parent->left;
+		parent->left->parent = new_node;
+	}
+	else
+	{
+		parent->left = new_node;
+	}
+
+	return (new_node);
+}
