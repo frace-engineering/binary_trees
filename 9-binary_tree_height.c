144aa6ca268ab398392ba64ef106ecee0f7dbc6b
Ugwu Friday B <fugwungene@gmail.com>
2023-11-01 18:41:13 +0000

    Fix betty issue

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,6 +1,9 @@
 #include "binary_trees.h"
-
-
+/**
+ * binary_tree_height - Calculates the height of a binary tree
+ * @tree: pointer to the root of the tree
+ * Return: height of the tree
+ */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t n, m;
